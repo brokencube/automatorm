11519a3c9ce567a518b8188690497fdbff4c30dc
Nik Barham <nick.barham@musicgateway.net>
2016-05-24 11:14:32 +0000

    Whoops - namespace clash

--- src/Automatorm/Exception/Query.php
+++ src/Automatorm/Exception/Query.php
@@ -1,11 +1,11 @@
 <?php
 namespace Automatorm\Exception;
 
-use Automatorm\Database\Query;
+use Automatorm\Database;
 
 class Query extends Database
 {
-	public function __construct(Query $query, \Exception $previous_exception = null)
+	public function __construct(Database\Query $query, \Exception $previous_exception = null)
 	{
 		parent::__construct('Query error: '.$query->error, $query, $previous_exception);
 	}
