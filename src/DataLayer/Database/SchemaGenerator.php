0a0d2099dc0a981055055ef5821cfa4c407b79b2
Nik Barham <nick.barham@musicgateway.net>
2017-08-22 10:59:56 +0000

    More changes to Schema for cross joints

--- src/DataLayer/Database/SchemaGenerator.php
+++ src/DataLayer/Database/SchemaGenerator.php
@@ -35,24 +35,36 @@ class SchemaGenerator implements SchemaGeneratorInterface
             ORDER BY b.table_name, b.constraint_name;"
         );
         $query->sql("
+            SELECT b.table_schema, b.table_name, b.column_name, b.referenced_table_schema, b.referenced_table_name, b.referenced_column_name
+            FROM information_schema.table_constraints a 
+            JOIN information_schema.key_column_usage b
+            ON a.table_schema = b.table_schema AND a.constraint_name = b.constraint_name
+            WHERE b.referenced_table_schema = database() AND a.constraint_type = 'FOREIGN KEY'
+            ORDER BY b.table_name, b.constraint_name;"
+        );
+        $query->sql("
             SELECT table_schema, table_name, column_name, data_type FROM information_schema.columns where table_schema = database();
         ");
         $query->sql("
             SELECT database() as `database`;
         ");
         
-        list($keys, $schema, $database) = $query->execute();
+        list($keys, $reversekeys, $schema, $database) = $query->execute();
         
         $database = $database[0]['database'];
         
         // Assemble list of table columns by table
         foreach ($schema as $row) {
             $tableName = Schema::normaliseCase($row['table_name']);
-            if (!$model[$tableName]) {
+            if (!array_key_exists($tableName, $model)) {
                 $model[$tableName]['table_schema'] = $row['table_schema'];
                 $model[$tableName]['table_name'] = $row['table_name'];
                 // All tables default to type 'table' - can also be 'pivot' or 'foreign' as detected later
                 $model[$tableName]['type'] = 'table';
+                $model[$tableName]['one-to-one'] = [];
+                $model[$tableName]['many-to-one'] = [];
+                $model[$tableName]['one-to-many'] = [];
+                $model[$tableName]['many-to-many'] = [];
             }
             
             // List all columns for this table
@@ -70,10 +82,6 @@ class SchemaGenerator implements SchemaGeneratorInterface
             if ($row['referenced_column_name'] == 'id' and $row['column_name'] == 'id') {
                 // If both columns in the key are 'id' then this is a 1 to 1 relationship.
                 // Create a link in both objects to each other
-                $model[$refTableName]['one-to-one'][Schema::underscoreCase($tableName)] = [
-                    'table' => $tableName,
-                    'schema' => $tableSchema
-                ];
                 $model[$tableName]['one-to-one'][Schema::underscoreCase($refTableName)] = [
                     'table' => $refTableName,
                     'schema' => $refTableSchema
@@ -87,6 +95,31 @@ class SchemaGenerator implements SchemaGeneratorInterface
                         'table' => $refTableName,
                         'schema' => $refTableSchema
                     ];
+                }
+            }
+        }
+        
+        // Loop over every foreign key definition
+        foreach ($reversekeys as $row) {
+            $tableName = Schema::normaliseCase($row['table_name']);
+            $tableSchema = $row['table_schema'];
+            
+            $refTableName = Schema::normaliseCase($row['referenced_table_name']);
+            $refTableSchema = $row['referenced_table_schema'];
+            
+            if ($row['referenced_column_name'] == 'id' and $row['column_name'] == 'id') {
+                // If both columns in the key are 'id' then this is a 1 to 1 relationship.
+                // Create a link in both objects to each other
+                if ($refTableSchema == $database) {
+                    $model[$refTableName]['one-to-one'][Schema::underscoreCase($tableName)] = [
+                        'table' => $tableName,
+                        'schema' => $tableSchema
+                    ];
+                }
+            } elseif ($row['referenced_column_name'] == 'id') {
+                // if this foreign key points at one 'id' column then this is a usable foreign 'key'
+                if (substr($row['column_name'], -3) == '_id') {
+                    $columnRoot = substr($row['column_name'], 0, -3);
                     
                     // Add the key constraint in reverse, trying to make a sensible name.
                     // If the column name was derived from the table name, just use the table name.
@@ -106,7 +139,7 @@ class SchemaGenerator implements SchemaGeneratorInterface
                     ];
                 }
             }
-        }
+        }        
         
         // Now look for pivot tables
         foreach ($model as $pivottablename => $pivot) {
@@ -137,7 +170,9 @@ class SchemaGenerator implements SchemaGeneratorInterface
                     }
                     
                     // Outersect of tables to create an array of all OTHER foreign keys in this table, for this foreign key.
-                    $othertables = array_values(array_diff_assoc($tableinfo, array($i => $table)));
+                    $othertables = $tableinfo;
+                    unset($othertables[$i]);
+                    $othertables = array_values($othertables);
                     
                     $model[ $table['table'] ][ 'many-to-many' ][ $propertyName ] = array(
                         'schema' => $table['schema'],
@@ -151,9 +186,11 @@ class SchemaGenerator implements SchemaGeneratorInterface
                 
                 // Remove the M-1 keys for these tables to fully encapsulate the M-M scheme.
                 foreach ($tableinfo as $table) {
-                    foreach ((array) $model[ $table['table'] ][ 'one-to-many' ] as $key => $val) {
-                        if ($val['table'] == $pivottablename) {
-                            unset($model[ $table['table'] ][ 'one-to-many' ][$key]);
+                    if ($table['schema'] == $database) {
+                        foreach ($model[ $table['table'] ][ 'one-to-many' ] as $key => $val) {
+                            if ($val['table'] == $pivottablename) {
+                                unset($model[ $table['table'] ][ 'one-to-many' ][$key]);
+                            }
                         }
                     }
                 }
